df7557dbad89e48aa9fca94634e402c4833ae948
Oscar Zhang <oscar.zth1999@gmail.com>
2019-12-22 00:58:34 -0400

    added test code for db

--- testing.py
+++ testing.py
@@ -5,6 +5,25 @@
 #                             farm_id=123,
 #                             filename='test/test_input.csv',
 #                             db_engine=None)
-import sqlalchemy
 import psycopg2
 
+try:
+    connection = psycopg2.connect(
+        user="postgres",
+        password="****",
+        host="127.0.0.1",
+        port="***",
+    )
+    cursor = connection.cursor()
+    query = "SELECT * FROM farm_datasource;"
+    cursor.execute(query)
+    print("Executed")
+    records = cursor.fetchall()
+    print(records)
+    '''
+    row () () () ()
+    reference
+    (<id>, <farm_id>, <software_id>, <software_version>, <file_location>, <datasource_type>, <script_name>, <script_arguments>, <active>)
+    '''
+except (Exception, psycopg2.Error) as err:
+    print(err)
